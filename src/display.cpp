6668f24b806ea0587ab8ee75d584786fefacf5fe
fcarlini <felipercarlini@gmail.com>
2021-09-14 21:04:57 -0300

    [ADD] format code

--- src/display.cpp
+++ src/display.cpp
@@ -15,7 +15,8 @@
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 TSPoint p;
 
-typedef union {
+typedef union
+{
     int16_t value;
     char byte[2];
 } eeprom_u;
@@ -44,7 +45,8 @@ void setupDisplay()
 // Touch
 void setupTouchScreen()
 {
-    if (shouldCalibrate()) {
+    if (shouldCalibrate())
+    {
         calibrate();
     }
     setCalibrationVlues();
@@ -65,45 +67,54 @@ void updateTsRaw()
 
 void calibrate()
 {
-        renderText(100, 100, 1, 3, " Presionar todo el contorno del display\n Luego reiniciar");
-        int16_t x_min, x_max, y_min, y_max;
-        bool calibrate_started=false;
-        while (true) {
-            updateTsRaw();
-            int x_vlue = p.y;
-            int y_vlue = p.x;
-            bool change_calibrate_vlues = false;
-            if (touchGetZ() >= MINPRESSURE) {
-                if (!calibrate_started) {
-                    x_min = x_vlue;
+    renderText(100, 100, 1, 3, " Presionar todo el contorno del display\n Luego reiniciar");
+    int16_t x_min, x_max, y_min, y_max;
+    bool calibrate_started = false;
+    while (true)
+    {
+        updateTsRaw();
+        int x_vlue = p.y;
+        int y_vlue = p.x;
+        bool change_calibrate_vlues = false;
+        if (touchGetZ() >= MINPRESSURE)
+        {
+            if (!calibrate_started)
+            {
+                x_min = x_vlue;
+                x_max = x_vlue;
+                y_min = y_vlue;
+                y_max = y_vlue;
+                calibrate_started = true;
+            }
+            else
+            {
+                if (x_vlue > x_max && x_vlue < 1023)
+                {
                     x_max = x_vlue;
-                    y_min = y_vlue;
+                    change_calibrate_vlues = true;
+                }
+                if (x_vlue < x_min && x_vlue < 1023)
+                {
+                    x_min = x_vlue;
+                    change_calibrate_vlues = true;
+                }
+                if (y_vlue > y_max && y_vlue < 1023)
+                {
                     y_max = y_vlue;
-                    calibrate_started = true;
-                } else {
-                    if (x_vlue > x_max && x_vlue < 1023) {
-                        x_max = x_vlue;
-                        change_calibrate_vlues = true;
-                    }
-                    if (x_vlue < x_min && x_vlue < 1023){
-                        x_min = x_vlue;
-                        change_calibrate_vlues = true;
-                    }
-                    if (y_vlue > y_max && y_vlue < 1023) {
-                        y_max = y_vlue;
-                        change_calibrate_vlues = true;
-                    }
-                    if (y_vlue < y_min && y_vlue < 1023) {
-                        y_min = y_vlue;
-                        change_calibrate_vlues = true;
-                    }
-                    if(change_calibrate_vlues) {
-                        writeCalibrateVluesEEPROM(x_min, x_max, y_min, y_max);
-                    }
-                 }
+                    change_calibrate_vlues = true;
+                }
+                if (y_vlue < y_min && y_vlue < 1023)
+                {
+                    y_min = y_vlue;
+                    change_calibrate_vlues = true;
+                }
+                if (change_calibrate_vlues)
+                {
+                    writeCalibrateVluesEEPROM(x_min, x_max, y_min, y_max);
+                }
             }
         }
-    
+    }
 }
 
 void writeCalibrateVluesEEPROM(int16_t x_min, int16_t x_max, int16_t y_min, int16_t y_max)
@@ -112,40 +123,44 @@ void writeCalibrateVluesEEPROM(int16_t x_min, int16_t x_max, int16_t y_min, int1
     writeEEPROM(pos, 1);
     pos++;
     writeIntEEPROM(pos, x_min);
-    pos=pos+2;
+    pos = pos + 2;
     writeIntEEPROM(pos, x_max);
-    pos=pos+2;
+    pos = pos + 2;
     writeIntEEPROM(pos, y_min);
-    pos=pos+2;
+    pos = pos + 2;
     writeIntEEPROM(pos, y_max);
     EEPROM.commit();
 }
 
-void writeIntEEPROM(unsigned int pos, int16_t value) {
+void writeIntEEPROM(unsigned int pos, int16_t value)
+{
     eeprom_u aux_union;
     aux_union.value = value;
     writeEEPROM(pos, aux_union.byte[0]);
-    writeEEPROM(pos+1, aux_union.byte[1]);
+    writeEEPROM(pos + 1, aux_union.byte[1]);
 }
 
 void writeEEPROM(unsigned int pos, char value)
-{    
-    if(EEPROM.read(pos) != value) {
+{
+    if (EEPROM.read(pos) != value)
+    {
         EEPROM.write(pos, value);
     }
 }
 
 bool shouldCalibrate()
 {
-    if(EEPROM.read(TS_EEPROM_SP) != 1) {
+    if (EEPROM.read(TS_EEPROM_SP) != 1)
+    {
         return true;
     }
     return false;
 }
 
-void setCalibrationVlues() {
+void setCalibrationVlues()
+{
     unsigned int pos = TS_EEPROM_SP;
-    pos ++;
+    pos++;
     calibrate_min_x = readIntEEPROM(pos);
     pos = pos + 2;
     calibrate_max_x = readIntEEPROM(pos);
@@ -155,13 +170,13 @@ void setCalibrationVlues() {
     calibrate_max_y = readIntEEPROM(pos);
 }
 
-int16_t readIntEEPROM(unsigned int pos) {
+int16_t readIntEEPROM(unsigned int pos)
+{
     eeprom_u aux_union;
     aux_union.byte[0] = EEPROM.read(pos);
-    aux_union.byte[1] = EEPROM.read(pos+1);
+    aux_union.byte[1] = EEPROM.read(pos + 1);
     return aux_union.value;
 }
-
 
 int touchGetX()
 {
